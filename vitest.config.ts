62181f1ef22997804ac5718a7110c27c380c4cef
Shaurya Patel <167566509+shauryapatel1@users.noreply.github.com>
2025-06-19 11:35:32 -0400

    Updated vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
+      reportsDirectory: './test-reports',
       exclude: [
         'node_modules/',
         'src/test/',
